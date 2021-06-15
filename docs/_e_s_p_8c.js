a86a36c6829013098e05c0d3ad429ec79c4c9e45
Naffik <76112481+Naffik@users.noreply.github.com>
2021-06-15 16:24:42 +0200

    Add files via upload

---
+++ docs/_e_s_p_8c.js
@@ -0,0 +1,18 @@
+var _e_s_p_8c =
+[
+    [ "gateway", "_e_s_p_8c.html#a315c572e404e2e6d7a2c8e456f938715", null ],
+    [ "local_IP", "_e_s_p_8c.html#aa60f30cbad0e034b3f48b2f9279c8308", null ],
+    [ "loop", "_e_s_p_8c.html#afe461d27b9c48d5921c00d521181f12f", null ],
+    [ "onWebSocketEvent", "_e_s_p_8c.html#aa029b646d66f083b953f61e4f6499993", null ],
+    [ "primaryDNS", "_e_s_p_8c.html#ad157e6083c3fa197d2e04576edd13ba5", null ],
+    [ "secondaryDNS", "_e_s_p_8c.html#aebb63a84e7ca5934f05d6ebec45d8233", null ],
+    [ "setup", "_e_s_p_8c.html#a4fc01d736fe50cf5b977f755b675f11d", null ],
+    [ "subnet", "_e_s_p_8c.html#a89cf134ada28d8ce0192d9fcbc83614a", null ],
+    [ "inPin", "_e_s_p_8c.html#a6c179d99655dcf5ce1700fcf256d9dfa", null ],
+    [ "outPin", "_e_s_p_8c.html#ad9003aed24b1f257ed3d4651cdf7909b", null ],
+    [ "password", "_e_s_p_8c.html#aa4a2ebcb494493f648ae1e6975672575", null ],
+    [ "servoM", "_e_s_p_8c.html#ac858883af801101bb306b0c9d7efc17d", null ],
+    [ "ssid", "_e_s_p_8c.html#a587ba0cb07f02913598610049a3bbb79", null ],
+    [ "val", "_e_s_p_8c.html#aa0ccb5ee6d882ee3605ff47745c6467b", null ],
+    [ "webSocket", "_e_s_p_8c.html#a5a9d65f1a8d9a12255db818887b5bcd3", null ]
+];
---
+++ docs/main_8c.js
@@ -0,0 +1,23 @@
+var main_8c =
+[
+    [ "RST_PIN", "main_8c.html#a36932b0e869e0114f32e255f61306d6b", null ],
+    [ "SS_PIN", "main_8c.html#a86fac98c9b4c98a3e50fc45440878391", null ],
+    [ "authorized", "main_8c.html#afd89940c594bc8f4f83077fe915332d4", null ],
+    [ "denied", "main_8c.html#acef0d6f1ec5e38b9262eb46b17bcd70e", null ],
+    [ "KP2", "main_8c.html#a6c38036d37a667f49b4e49458f71297d", null ],
+    [ "lcd", "main_8c.html#ae084e1bc8ccb35ea289ba0ca4972ea6d", null ],
+    [ "loop", "main_8c.html#afe461d27b9c48d5921c00d521181f12f", null ],
+    [ "mfrc522", "main_8c.html#ac672f817299d07cc428fe3f456235273", null ],
+    [ "pass_c", "main_8c.html#a5986cfc798336c3645d184d50f558208", null ],
+    [ "pass_i", "main_8c.html#af93e001edc3a60fab6fffd9c06f12473", null ],
+    [ "setup", "main_8c.html#a4fc01d736fe50cf5b977f755b675f11d", null ],
+    [ "card", "main_8c.html#acd789e381a684163a021e2d228653afd", null ],
+    [ "check", "main_8c.html#a51c6e5d8fbed1c6a97ea56488d4f1ba4", null ],
+    [ "esp", "main_8c.html#acce609a920ca599df40542a3b1da7325", null ],
+    [ "inData", "main_8c.html#a76a30907552fcea8f94bdd99fa57f74d", null ],
+    [ "j", "main_8c.html#a37d972ae0b47b9099e30983131d31916", null ],
+    [ "KEYS", "main_8c.html#aba109ab7392d21bbbff2e4f23338d461", null ],
+    [ "outData", "main_8c.html#a9048d83f2da59c3a06b69a8eacc5cff4", null ],
+    [ "password", "main_8c.html#a3f0a1b00c5b5bc7ec60b5cc784102225", null ],
+    [ "val", "main_8c.html#aa0ccb5ee6d882ee3605ff47745c6467b", null ]
+];
